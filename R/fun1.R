aa99efd6662208df1a2bb6a0d764976291d88320
j-kain <jonathon.k.martinez@outlook.com>
2022-03-09 18:14:27 -0800

    another commit

--- R/fun1.R
+++ R/fun1.R
@@ -1,1 +1,12 @@
-print("this is a function")
+#' Title
+#'
+#' @param num A number to add to 5
+#'
+#' @return A number plus 5
+#' @export
+#'
+#' @examples
+#' add5(5)
+add5 <- function(num){
+    num + 5
+}
